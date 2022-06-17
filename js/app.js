b92f3491f0dc3282ca2d556c87311632eb093d35
Chidinma Udo <97461848+Mackenzie-OO7@users.noreply.github.com>
2022-06-17 18:30:00 +0100

    updated app.js

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ latestPosts();
 //DELETE Posts
 function latestUI (postsUI) {
     let latestPostHolder = "";
-        console.log(latestBox);
+      
 
         postsUI.forEach(latestPost => {
             let wordLength = latestPost.body;
@@ -28,16 +28,16 @@ function latestUI (postsUI) {
             titleLength = titleLength.slice(0, 30);
 
             latestPostHolder += `
-            <div class="col-lg-5 col-md-12 mb-5 mx-auto">
-            <div class="post-body">
+            <div class=" mb-5 mx-auto col-lg-5 col-md-12">
+            <div class="card-body">
                 <div class="img-post">
                     <img src="img/post${latestPost.id}.jpg" alt="image" style="border-radius:15px; height: 33vmax !important;" class="img-fluid w-100 post-images">
                 </div>
-                <h5 class="latestPost-title pt-2 pb-2 main-color fw-bold">${titleLength}</h5>
-                <p class="latestPost-body">${wordLength}</p>
+                <h5 class="latestcard-title pt-2 pb-2 fw-bold">${titleLength}</h5>
+                <p class="latestcard-body">${wordLength}</p>
                 <div class="btn-div d-flex justify-content-between ">
                     <button onclick="updatelatestPost(${latestPost.id})" class="btn btn-dark" href="#form-section"><i class="bi bi-pencil mx-1"></i>Update</button>
-                    <button class="btn btn-warning btn-outline-dark ms-3" onclick="viewlatest(${latestPost.id})"><i class="bi bi-book mx-1"></i>View</button>
+                    <button class="btn btn-warning btn-outline-dark ms-3" onclick="viewlatestPost(${latestPost.id})"><i class="bi bi-book mx-1"></i>View</button>
                     <button class="btn btn-danger btn-outline-light ms-3" onclick="deletelatestPost(${latestPost.id})"><i class="bi bi-trash mx-1"></i>Delete</button>
                 </div>
             </div>
@@ -55,16 +55,28 @@ function deletelatestPost(id) {
         .then((data) => {
             console.log(data)
             latestBox = latestBox.filter(latestPost => latestPost.id !== id)
-            console.log(latestBox)
-            // use a function to display the UI
             latestUI(latestBox) ;
         })
 
 }
+
+// VIEW Posts
+
+function viewlatestPost(id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+            localStorage.setItem('viewedPost', JSON.stringify(data))
+            window.location.href = 'nextpage.html';
+            // console.log(data)
+        });
+}
+
+
+   
+
 // UPDATE Posts
 function updatelatestPost(id) {
-    console.log(id)
-
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
@@ -80,10 +92,10 @@ function updatelatestPost(id) {
         .then((response) => response.json())
         .then((data) => {
 
-            console.log(data)
-            let postTitles = document.querySelectorAll('.latestPost-title') // 100 post titles [0 -99]
-            let postBodies = document.querySelectorAll('.latestPost-body')
-            console.log(postTitles)
+            // console.log(data)
+            let postTitles = document.querySelectorAll('.latestCard-title') // 100 post titles [0 -99]
+            let postBodies = document.querySelectorAll('.latestCard-body')
+            // console.log(postTitles)
             postTitles.forEach((postTitle, index) => {
                 if (index + 1 === id) {
                     if (data.title !== "") {
@@ -101,8 +113,7 @@ function updatelatestPost(id) {
                 }
 
             })
-
-    });
+     });
 }
 
 
@@ -135,12 +146,12 @@ function createPost(e) {
             latestPostBox.forEach(post => {
                 latestPostHolder += `
                     <div class="col-lg-4 col-md-6 mb-5">
-                        <div class="post-body">
+                        <div class="card-body">
                             <div class="img-post">
                                 <img src="img/post${post.id}.jpg" alt="image" style="border-radius:15px; height: 33vmax !important;" class="img-fluid w-100 post-images">
                             </div>
-                            <h5 class="post-title pt-2 pb-2 main-color fw-bold">${post.title}</h5>
-                            <p class="post-body">${post.body}</p>
+                            <h5 class="card-title pt-2 pb-2  fw-bold">${post.title}</h5>
+                            <p class="card-body">${post.body}</p>
                             <div class="btn-div d-flex justify-content-between ">
                                 <button onclick="updatePost(${post.id})" class="btn btn-dark" href="#form-section"><i class="bi bi-pencil mx-1"></i> Update</button>
                                 <button class="btn btn-warning btn-outline-dark ms-3" onclick="view(${post.id})"> <i class="bi bi-book mx-1"></i>View</button>
